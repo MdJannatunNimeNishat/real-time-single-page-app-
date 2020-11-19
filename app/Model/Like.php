f95c8d0003a0509eb03174cb518b5493495a8a2c
MdJannatunNimeNishat <baust.cse.160201077@gmail.com>
2020-11-20 00:35:11 +0600

    Factory and seed (error in Likes (  Argument 1 passed to Illuminate\Database\Eloquent\Relations\HasOneOrMany::save() ))

--- app/Model/Like.php
+++ app/Model/Like.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+     protected $fillable = [
+        'reply_id', 'user_id'
+    ];
+
+    public function reply(){
+    	return $this->belongesTo(Reply::class);
+    }
 }
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -3,10 +3,20 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Reply;
+
 use Faker\Generator as Faker;
+use App\Model\Question;
+use App\User;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body'=> $faker->text,
+
+        'question_id'=> function(){
+        	return Question::all()->random();
+        },
+        'user_id'=> function(){
+        	return User::all()->random();
+        }
     ];
 });
